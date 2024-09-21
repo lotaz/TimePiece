5a6166506089810588c45848a91ca6ef7d9a4ff9
Hoang-N-D-1 <ngusimeri1@gmail.com>
2024-09-21 08:50:09 +0700

    fix-error-cant-update-profile

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -8,15 +8,45 @@ interface UpdateUserProFile {
   birthday: string
   citizenID: string
 }
-
 export const updateUserProFileService = async (
   params: UpdateUserProFile,
   userId: number
 ) => {
   try {
+    const form = new FormData()
+
+    if (params.name) {
+      form.append('name', `${params.name}`)
+    }
+
+    if (params.address) {
+      form.append('address', `${params.address}`)
+    }
+
+    if (params.phoneNumber) {
+      form.append('phoneNumber', `${params.phoneNumber}`)
+    }
+
+    if (params.gender) {
+      form.append('gender', `${params.gender}`)
+    }
+
+    if (params.birthday) {
+      form.append('birthday', `${params.birthday}`)
+    }
+
+    if (params.citizenID) {
+      form.append('citizenID', `${params.citizenID}`)
+    }
+
     const response = await axiosClient.put(
       `api/users/${userId}/UpdateUserProfile`,
-      params
+      form,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
     )
 
     return response.data
