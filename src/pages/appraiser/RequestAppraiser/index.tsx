5c261d8546ae4cf14cacffa8d2689af03c0bc97c
lota <tidihoc123@gmail.com>
2024-09-27 07:34:01 +0700

    done flow create appraisal reqeust

--- src/pages/appraiser/RequestAppraiser/index.tsx
+++ src/pages/appraiser/RequestAppraiser/index.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material'
 import FindRequestAppraiser from './components/FindRequestAppraiser'
 import ListRequest from './components/ListRequest'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AppraiserLayout from '@/components/Layout/AppraiserLayout'
 import useSWR from 'swr'
 import { AppPath } from '@/services/utils'
@@ -10,18 +10,20 @@ import { RequestAppraisal } from './type'
 const PAGE_SIZE = 6
 
 const RequestAppraiserPage: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(1) // Track current display page (starts at 1)
+  const [currentPage, setCurrentPage] = useState(0) // Start page from 0
   const [loadedPages, setLoadedPages] = useState<RequestAppraisal[]>([]) // Stores all fetched appraisal data
   const [totalPages, setTotalPages] = useState(1) // Total number of pages for pagination
+  const [isPageLoading, setIsPageLoading] = useState(true) // Loading state for the first page load
 
   // Fetch data using SWR based on the current page
-  const { isLoading } = useSWR(
-    `${AppPath.GET_APPRAISAL_REQUESTS}?page=${Math.ceil(loadedPages.length / PAGE_SIZE) + 1}&size=${PAGE_SIZE}`,
+  const { data, isValidating } = useSWR(
+    `${AppPath.GET_APPRAISAL_REQUESTS}?page=${Math.ceil(loadedPages.length / PAGE_SIZE)}&size=${PAGE_SIZE}`,
     {
       revalidateOnFocus: false,
       onSuccess: (data) => {
         setTotalPages(data.totalPages)
         setLoadedPages((prevAppraisals) => [...prevAppraisals, ...data.content])
+        setIsPageLoading(false) // Turn off loading after the first page is loaded
       }
     }
   )
@@ -34,10 +36,18 @@ const RequestAppraiserPage: React.FC = () => {
     setCurrentPage(page)
   }
 
+  // Load the next page in the background when the user changes the page
+  useEffect(() => {
+    if (currentPage * PAGE_SIZE >= loadedPages.length && !isValidating) {
+      // If user requests a page that's not loaded, load the next set
+      setIsPageLoading(true)
+    }
+  }, [currentPage, loadedPages.length, isValidating])
+
   // Calculate the items to display based on the current page
   const displayedAppraisals = loadedPages.slice(
-    (currentPage - 1) * 5,
-    currentPage * 5
+    currentPage * PAGE_SIZE,
+    currentPage * PAGE_SIZE + PAGE_SIZE
   )
 
   return (
@@ -46,10 +56,10 @@ const RequestAppraiserPage: React.FC = () => {
         <FindRequestAppraiser />
         <ListRequest
           requests={displayedAppraisals} // Display items for the current page
-          currentPage={currentPage}
+          currentPage={currentPage} // Pagination UI is 1-based
           totalPages={totalPages}
           onPageChange={handlePageChange}
-          isLoading={isLoading} // Pass the loading state
+          isLoading={isPageLoading} // Pass the loading state
         />
       </Box>
     </AppraiserLayout>
